826d08bc85862fa8bda6bf4c8189ebf4827d3169
Houda <fn_khettache@esi.dz>
2021-11-18 10:50:55 +0100

    Build

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
 import { InfoSection, Pricing } from "../../components";
-import Biography from "../Biography/Biography1";
 import InfoSectionRight from "../../components/InfoSectionRight/InfoSectionRight";
 function Home() {
   return (
